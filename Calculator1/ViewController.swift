ded4fde79fcae2fd637a56778ab5e67e98432771
Deep Mehta <dmehta35@my.centennialcollege.ca>
2022-09-25 09:34:07 -0400

    calculator app for iOS, UI design

---
+++ Calculator1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator1
+//
+//  Created by Parth Maru on 2022-09-25.
+//  301209761
+
+// CALCULATOR APPLICATION
+// DEEP HARISHKUMAR MEHTA - 301212407
+// KHUSHBOO KODWANI -
+
+// We Created a basic calculator application for iOS that includes different operator buttons, number keys, delete key, backspace key and decimal key.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
